4ed8a19af809ed8ddd34b750bdc3e1d800e0c914
dastanumar <dastanattokuruulu@yahoo.com>
2018-03-29 16:20:52 -0500

    f

--- src/test/java/com/cybertek/TikiWiki/utilities/Browser.java
+++ src/test/java/com/cybertek/TikiWiki/utilities/Browser.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
@@ -41,6 +42,11 @@ public class Browser {
 		case "firefox":
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
+			break;
+			
+		case "opera":
+			WebDriverManager.operadriver().setup();
+			driver = new OperaDriver();
 			break;
 			
 		case "ied":
